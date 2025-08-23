6c0c060611d76539975bbde313fa9792c3057b85
MaChau <2654427647@qq.com>
2025-08-24 00:15:39 +0800

    feat: introduce Captain's Parrot minion with battlecry ability to draw a pirate from the deck

---
+++ src/captains-parrot/index.ts
@@ -0,0 +1,49 @@
+// Captain's Parrot
+// Pirates and Parrots go together like Virmen and Carrots.
+// Battlecry: Draw a Pirate from your deck.
+// Type: Minion
+// Minion Type: Beast
+// Rarity: Epic
+// Set: Legacy
+// Class: Neutral
+// Cost to Craft: 400 / 1600 (Golden)
+// Disenchanting Yield: 100 / 400 (Golden)
+// Artist: Daren Bader
+// Collectible
+
+import { AttackModel, ClassType, HealthModel, MinionModel, RaceType, RarityType, RoleModel, CardHooksModel } from "hearthstone-core";
+import { CaptainsParrotBattlecryModel } from "./battlecry";
+import { CostModel } from "hearthstone-core";
+
+export class CaptainsParrotModel extends MinionModel {
+    constructor(props: CaptainsParrotModel['props']) {
+        super({
+            uuid: props.uuid,
+            state: {
+                name: 'Captain\'s Parrot',
+                desc: 'Battlecry: Draw a Pirate from your deck.',
+                races: [RaceType.BEAST],
+                flavorDesc: 'Pirates and Parrots go together like Virmen and Carrots.',
+                rarity: RarityType.EPIC,
+                class: ClassType.NEUTRAL,
+                ...props.state,
+            },
+            child: {
+                cost: new CostModel({ state: { origin: 2 }}),
+                role: new RoleModel({
+                    child: {
+                        attack: new AttackModel({ state: { origin: 1 }}),
+                        health: new HealthModel({ state: { origin: 1 }}), 
+                    }
+                }),
+                hooks: new CardHooksModel({
+                    child: {
+                        battlecry: [new CaptainsParrotBattlecryModel({})]
+                    }
+                }),
+                ...props.child,
+            },
+            refer: { ...props.refer },
+        });
+    }
+} 
