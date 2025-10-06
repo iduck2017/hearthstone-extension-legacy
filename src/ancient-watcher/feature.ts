514ebef6576a3c5bd2982a6b5b73bf093bac2f8e
MaChau <2654427647@qq.com>
2025-10-06 09:02:11 +0800

    feat: implement Ancient Watcher minion model with restriction to prevent attacking, along with comprehensive tests for gameplay mechanics

---
+++ src/ancient-watcher/feature.ts
@@ -0,0 +1,44 @@
+import { FeatureModel, ROLE_ROUTE, RoleActionDecor, RoleActionModel, RoleActionProps, RoleRoute } from "hearthstone-core";
+import { StateUtil, StoreUtil, Loader, Decor } from "set-piece";
+
+export namespace AncientWatcherFeatureProps {
+    export type E = {};
+    export type S = {};
+    export type C = {};
+    export type R = {}
+    export type P = RoleRoute
+}
+
+@StoreUtil.is('ancient-watcher-feature')
+export class AncientWatcherFeatureModel extends FeatureModel<
+    AncientWatcherFeatureProps.E,
+    AncientWatcherFeatureProps.S,
+    AncientWatcherFeatureProps.C,
+    AncientWatcherFeatureProps.R,
+    AncientWatcherFeatureProps.P
+> {
+    constructor(loader?: Loader<AncientWatcherFeatureModel>) {
+        super(() => {
+            const props = loader?.() ?? {};
+            return {
+                uuid: props.uuid,
+                state: {
+                    name: 'Ancient Watcher\'s Restriction',
+                    desc: 'Can\'t attack.',
+                    isActive: true,
+                    ...props.state,
+                },
+                child: { ...props.child },
+                refer: { ...props.refer },
+                route: ROLE_ROUTE,
+            }
+        });
+    }
+
+    // Disable attack action
+    @StateUtil.on(self => self.route.role?.proxy.child.action.decor)
+    private onCheck(that: RoleActionModel, decor: RoleActionDecor) {
+        if (!this.state.isActive) return;
+        decor.lock()
+    }
+} 
---
+++ src/ancient-watcher/index.ts
@@ -0,0 +1,51 @@
+/*
+ * Ancient Watcher
+ * Why do its eyes seem to follow you as you walk by?
+ * Can't attack.
+ * Type: Minion
+ * Rarity: Rare
+ * Set: Legacy
+ * Class: Neutral
+ * Cost to Craft: 100 / 800 (Golden)
+ * Disenchanting Yield: 20 / 100 (Golden)
+ * Artist: Richard Wright
+ * Collectible
+ */
+
+import { RoleAttackModel, ClassType, RoleHealthModel, MinionCardModel, RarityType, RoleModel, LibraryUtil, CostModel } from "hearthstone-core";
+import { AncientWatcherFeatureModel } from "./feature";
+import { Loader } from "set-piece";
+
+@LibraryUtil.is('ancient-watcher')  
+export class AncientWatcherModel extends MinionCardModel {
+    constructor(loader?: Loader<AncientWatcherModel>) {
+        super(() => {
+            const props = loader?.() ?? {};
+            return {
+                uuid: props.uuid,
+                state: {
+                    name: 'Ancient Watcher',
+                    desc: 'Can\'t attack.',
+                    isCollectible: true,
+                    flavorDesc: 'Why do its eyes seem to follow you as you walk by?',
+                    rarity: RarityType.RARE,
+                    class: ClassType.NEUTRAL,
+                    races: [],
+                    ...props.state,
+                },
+                child: {
+                    cost: new CostModel(() => ({ state: { origin: 2 }})),
+                    role: new RoleModel(() => ({
+                        child: {
+                            attack: new RoleAttackModel(() => ({ state: { origin: 4 }})),
+                            health: new RoleHealthModel(() => ({ state: { origin: 5 }})), 
+                            features: [new AncientWatcherFeatureModel()]
+                        }
+                    })),
+                    ...props.child,
+                },
+                refer: { ...props.refer },
+            }
+        });
+    }
+} 
