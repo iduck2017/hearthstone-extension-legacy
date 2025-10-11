b68183704509724f2943059eab2477c5de9598ef
MaChau <2654427647@qq.com>
2025-10-11 16:17:42 +0800

    refactor: standardize GameModel constructor implementations across multiple test files to consistently utilize props parameter for improved clarity and maintainability

--- src/twilight-drake/index.test.ts
+++ src/twilight-drake/index.test.ts
@@ -12,19 +12,19 @@ import { WispModel } from "../wisp";
 import { boot } from "../boot";
 
 describe('twilight-drake', () => {
-    const game = new GameModel(() => ({
+    const game = new GameModel({
         state: { debug: { isDrawDisabled: true }},
         child: {
-            playerA: new PlayerModel(() => ({
+            playerA: new PlayerModel({
                 child: {
-                    mana: new ManaModel(() => ({ state: { origin: 10 }})),
+                    mana: new ManaModel({ state: { origin: 10 }}),
                     hero: new MageModel(),
-                    board: new BoardModel(() => ({
+                    board: new BoardModel({
                         child: { 
                             minions: []
                         }
-                    })),
-                    hand: new HandModel(() => ({
+                    }),
+                    hand: new HandModel({
                         child: { 
                             minions: [
                                 new TwilightDrakeModel(), 
@@ -33,31 +33,31 @@ describe('twilight-drake', () => {
                             ],
                             spells: []
                         }
-                    })),
-                    deck: new DeckModel(() => ({
+                    }),
+                    deck: new DeckModel({
                         child: { minions: [] }
-                    }))
+                    })
                 }
-            })),
-            playerB: new PlayerModel(() => ({
+            }),
+            playerB: new PlayerModel({
                 child: {
-                    mana: new ManaModel(() => ({ state: { origin: 10 }})),
+                    mana: new ManaModel({ state: { origin: 10 }}),
                     hero: new MageModel(),
-                    board: new BoardModel(() => ({
+                    board: new BoardModel({
                         child: { 
                             minions: []
                         }
-                    })),
-                    hand: new HandModel(() => ({
+                    }),
+                    hand: new HandModel({
                         child: { 
                             minions: [],
                             spells: []
                         }
-                    }))
+                    })
                 }
-            }))
+            })
         }
-    }));
+    });
     boot(game);
 
     const playerA = game.child.playerA;
--- src/young-dragonhawk/index.test.ts
+++ src/young-dragonhawk/index.test.ts
@@ -12,48 +12,48 @@ import { ShieldbearerModel } from "../shieldbearer";
 import { boot } from "../boot";
 
 describe('young-dragonhawk', () => {
-    const game = new GameModel(() => ({
+    const game = new GameModel({
         state: { debug: { isDrawDisabled: true }},
         child: {
-            playerA: new PlayerModel(() => ({
+            playerA: new PlayerModel({
                 child: {
-                    mana: new ManaModel(() => ({ state: { origin: 10 }})),
+                    mana: new ManaModel({ state: { origin: 10 }}),
                     hero: new MageModel(),
-                    board: new BoardModel(() => ({
+                    board: new BoardModel({
                         child: { 
                             minions: [new YoungDragonhawkModel()]
                         }
-                    })),
-                    hand: new HandModel(() => ({
+                    }),
+                    hand: new HandModel({
                         child: { 
                             minions: [],
                             spells: []
                         }
-                    })),
-                    deck: new DeckModel(() => ({
+                    }),
+                    deck: new DeckModel({
                         child: { minions: [] }
-                    }))
+                    })
                 }
-            })),
-            playerB: new PlayerModel(() => ({
+            }),
+            playerB: new PlayerModel({
                 child: {
-                    mana: new ManaModel(() => ({ state: { origin: 10 }})),
+                    mana: new ManaModel({ state: { origin: 10 }}),
                     hero: new MageModel(),
-                    board: new BoardModel(() => ({
+                    board: new BoardModel({
                         child: { 
                             minions: [new ShieldbearerModel()]
                         }
-                    })),
-                    hand: new HandModel(() => ({
+                    }),
+                    hand: new HandModel({
                         child: { 
                             minions: [],
                             spells: []
                         }
-                    }))
+                    })
                 }
-            }))
+            })
         }
-    }));
+    });
     boot(game);
 
     const playerA = game.child.playerA;
